a0b5b2cb88505a432b939eaed4e5acea7d710838
Gallagher Polyn <gallagher.polyn@gmail.com>
2018-04-21 15:30:39 -0400

    Docker-ready

--- client/polyfills.js
+++ client/polyfills.js
@@ -8,6 +8,10 @@ console.log('Load your polyfills')
 // core-js comes with Next.js. So, you can import it like below
 import includes from 'core-js/library/fn/string/virtual/includes'
 import repeat from 'core-js/library/fn/string/virtual/repeat'
+require('core-js/library/fn/object/assign')
+// import 'core-js/library/es6/object'
+import 'core-js/library/es6/map'
+import 'core-js/library/es6/set'
 
 String.prototype.includes = includes
 String.prototype.repeat = repeat
--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,22 @@ const logoSVG = require('../static/images/logo.svg')
 const logoPNG = require('../static/images/logo.png')
 const txt = require('../static/file.txt')
 
+const object1 = {
+  a: 1,
+  b: 2,
+  c: 3
+};
+
+const object2 = Object.assign({c: 4, d: 5}, object1);
+
+
+
+console.log(object2.c, object2.d);
+
+const set1 = new Set([1, 2, 3, 4, 5]);
+
+console.log(set1.has(1));
+
 export default () => (
   <div>
     <strong>{txt}</strong>
